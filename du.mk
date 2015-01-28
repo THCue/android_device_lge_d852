e8bbe745fcdb1667a46967732705c5d4708647c5
Zane <zane.stewart@gmail.com>
2015-01-27 16:53:37 -0800

    Update and rename slim.mk to du.mk
    fix for DU

--- du.mk
+++ du.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/lge/d852/full_d852.mk)
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1440
 TARGET_SCREEN_HEIGHT := 2560
 
 PRODUCT_RELEASE_NAME := LG G3
-PRODUCT_NAME := slim_d852
+PRODUCT_NAME := du_d852
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
